5745b0621d30f7a03a7346d12a05f5d747ea3388
vkshatri <107268303+vkshatri@users.noreply.github.com>
2022-07-25 11:59:32 +0100

    Updated to share/unshare the application record

--- SDDS.Workflow/Consultation/ShareApplicationRecord.cs
+++ SDDS.Workflow/Consultation/ShareApplicationRecord.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Sdk.Workflow;
@@ -14,10 +14,10 @@ namespace SDDS.Workflow.Consultation
         [ReferenceTarget("sdds_application")]
         public InArgument<EntityReference> ApplicationRef { get; set; }
 
-        [RequiredArgument]
         [Input("TeamRef")]
         [ReferenceTarget("team")]
         public InArgument<EntityReference> TeamRef { get; set; }
+
 
         protected override void Execute(CodeActivityContext executionContext)
         {
@@ -26,29 +26,25 @@ namespace SDDS.Workflow.Consultation
             IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
             IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
             ITracingService tracingService = (ITracingService)executionContext.GetExtension<ITracingService>();
-
-
-            tracingService.Trace("Entering Checking task workflow");
+            tracingService.Trace("Entering workflow");
             try
             {
-                EntityReference application = ApplicationRef.Get<EntityReference>(executionContext);
-                if (application == null || application.LogicalName != "sdds_application") return;
-
-                EntityReference team = TeamRef.Get<EntityReference>(executionContext);
-                if (team == null || team.LogicalName != "team") return;
-
-                // Grant the team read/write access to the lead.
-                var grantAccessRequest = new GrantAccessRequest
+                if (context.InputParameters.Contains("Target"))
                 {
-                    PrincipalAccess = new PrincipalAccess
+                    Entity target = (Entity)context.InputParameters["Target"];
+                    if (target == null) return;
+                    switch (target.LogicalName)
                     {
-                        AccessMask = AccessRights.ReadAccess | AccessRights.WriteAccess,
-                        Principal = team
-                    },
-                    Target = application
-                };
+                        case "sdds_application":
+                            ShareUnshareApplicationToAdvisor(target, context, service, tracingService);
+                            break;
+                        case "sdds_consultation":
+                            ShareApplicationRecordToConsulting(executionContext, service);
+                            break;
 
-                service.Execute(grantAccessRequest);
+                    }
+
+                }
 
             }
             catch (Exception ex)
@@ -57,6 +53,114 @@ namespace SDDS.Workflow.Consultation
 
                 throw new InvalidWorkflowException("Failure in custom workflow action: " + ex.Message);
             }
+        }
+
+        /// <summary>
+        /// Share the application record to the consulting team.
+        /// </summary>
+        /// <param name="executionContext"></param>
+        /// <param name="service"></param>
+        private void ShareApplicationRecordToConsulting(CodeActivityContext executionContext, IOrganizationService service)
+        {
+            EntityReference application = ApplicationRef.Get<EntityReference>(executionContext);
+            if (application == null || application.LogicalName != "sdds_application") return;
+
+            EntityReference team = TeamRef.Get<EntityReference>(executionContext);
+            if (team == null || team.LogicalName != "team") return;
+
+            // Grant the team read/write access to the lead.
+            ShareRecord(application, team, service);
+
+        }
+
+        /// <summary>
+        /// Shares/UnShare the application record with Advisor/Lead Advisor.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="context"></param>
+        /// <param name="service"></param>
+        private void ShareUnshareApplicationToAdvisor(Entity target, IWorkflowContext context,
+            IOrganizationService service, ITracingService tracingService)
+        {
+            tracingService.Trace("Entering ShareUnshareApplicationToAdvisor");
+            EntityReference advisor = null;
+            EntityReference oldAdvisor = null;
+            EntityReference oldLeadAdvisor = null;
+            EntityReference leadAdvisor = null;
+
+            if (target.Attributes.Contains("sdds_assessorid"))
+            {
+                advisor = target.GetAttributeValue<EntityReference>("sdds_assessorid");
+                ShareRecord(target.ToEntityReference(), advisor, service);
+
+            }
+            if (target.Attributes.Contains("sdds_leadadvisor"))
+            {
+                leadAdvisor = target.GetAttributeValue<EntityReference>("sdds_leadadvisor");
+                ShareRecord(target.ToEntityReference(), leadAdvisor, service);
+
+            }
+            if (context.PreEntityImages.ContainsKey("PreBusinessEntity"))
+            {
+                Entity preImage = context.PreEntityImages["PreBusinessEntity"];
+                if (preImage != null && preImage.LogicalName == "sdds_application")
+                {
+                    oldAdvisor = preImage.GetAttributeValue<EntityReference>("sdds_assessorid");
+                    oldLeadAdvisor = preImage.GetAttributeValue<EntityReference>("sdds_leadadvisor");
+
+                }
+                if (advisor != null && oldAdvisor != null)
+                {
+                    if (advisor.Id != oldAdvisor.Id)
+                        UnShareRecord(target.ToEntityReference(), oldAdvisor, service);
+
+                }
+                if (leadAdvisor != null && oldLeadAdvisor != null)
+                {
+                    if (leadAdvisor.Id != oldLeadAdvisor.Id)
+                        UnShareRecord(target.ToEntityReference(), oldLeadAdvisor, service);
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Share the record to user or team.
+        /// </summary>
+        /// <param name="application"></param>
+        /// <param name="teamOrUser"></param>
+        /// <param name="service"></param>
+        private void ShareRecord(EntityReference application, EntityReference teamOrUser, IOrganizationService service)
+        {
+            var grantAccessRequest = new GrantAccessRequest
+            {
+                PrincipalAccess = new PrincipalAccess
+                {
+                    AccessMask = AccessRights.ReadAccess | AccessRights.WriteAccess,
+                    Principal = teamOrUser
+                },
+                Target = application
+            };
+
+            service.Execute(grantAccessRequest);
+        }
+
+        /// <summary>
+        /// Unshare record from user or team.
+        /// </summary>
+        /// <param name="application"></param>
+        /// <param name="teamOrUser"></param>
+        /// <param name="service"></param>
+        private void UnShareRecord(EntityReference application, EntityReference teamOrUser, IOrganizationService service)
+        {
+            // Revoke access to the lead for the second user.
+            var revokeUser2AccessReq = new RevokeAccessRequest
+            {
+                Revokee = teamOrUser,
+                Target = application
+            };
+
+            service.Execute(revokeUser2AccessReq);
         }
     }
 }
