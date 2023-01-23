c405927a117f93f08f571b243011314e5d8c9266
Vikram kshatri <vikram.kshatri@capgemini.com>
2023-01-23 16:45:42 +0000

    Unit test cases added.

--- SDDS.UnitTests/AddressLookup/AddressLookupResponseTests.cs
+++ SDDS.UnitTests/AddressLookup/AddressLookupResponseTests.cs
@@ -1,45 +1,60 @@
-﻿
-using FakeItEasy.Sdk;
-using FakeXrmEasy;
+﻿using FakeXrmEasy.Abstractions;
 using FakeXrmEasy.Plugins;
 using FluentAssertions;
 using Microsoft.Xrm.Sdk;
 using SDDS.Plugin.GetAddressForPostCode;
 using System;
-using System.Diagnostics;
-using System.IdentityModel.Metadata;
-using System.Runtime.CompilerServices;
-using System.Security.Principal;
-using System.Xml.Linq;
+using System.Collections.Generic;
 using Xunit;
 
 namespace SDDS.UnitTests.AddressLookup
 {
     public class AddressLookupResponseTests : FakeXrmEasyTestsBase
     {
+        readonly IXrmFakedContext fakecontext;
+
+        public AddressLookupResponseTests()
+        {
+            fakecontext = this.xrmFakedContext;
+
+
+        }
         [Fact]
 
-        public void ExecuteBusinessLogic()
+        public void ExecuteBusinessLogic_get_address_by_postcode()
         {
             // Prepare context
 
-            var fakecontext = this.xrmFakedContext;
-
-             var iTracing = new XrmFakedTracingService();
-
-            // Arrange
-             var targetEntityId = new Guid(); ;
-
-            // Define target with expected Attributes
-
-
-            // Define Input Parameter
-
             var inputParameter = new ParameterCollection
             {
-               
+
                 { "PostCode", "TW34AP" }
             };
+
+            var generalconfiguration = new Entity("sdds_generalconfiguration");
+            generalconfiguration.Id = Guid.NewGuid();
+            generalconfiguration["sdds_configurationkey"] = "Passphrase";
+            generalconfiguration["sdds_value"] = "passpharase@1234";
+
+            EntityCollection configdata = new EntityCollection();
+            configdata.Entities.Add(generalconfiguration);
+            generalconfiguration = new Entity("sdds_generalconfiguration");
+            generalconfiguration.Id = Guid.NewGuid();
+            generalconfiguration["sdds_configurationkey"] = "AddressApiHostUrl";
+            generalconfiguration["sdds_value"] = "https://getaddressbyposrcode.org";
+            configdata.Entities.Add(generalconfiguration);
+
+            var generalconfigForSecureFile = new Entity("sdds_generalconfiguration");
+            generalconfigForSecureFile.Id = Guid.NewGuid();
+            generalconfigForSecureFile["sdds_configurationkey"] = "SecureCertificateFile1";
+
+            var notes = new Entity("annotation");
+            notes.Id = Guid.NewGuid();
+            notes["filename"] = "TestFile.pfx";
+            notes["subject"] = "The certificate file for Address Lookup API";
+            notes["annotationid"] = notes.Id;
+            notes["documentbody"] = "Qk5HIDogU2Vzc2lvbiB3aXRoIFNhdHlhCgotIDMgYXBwLCBsYW5kIG93bmVyIHBvcnRhbCwgZGV2ZWxvcGVyIHBvcnRhbAotIHNwYXJrbGUgdG8gZGVwbG95IGx1Z2luLCB4cm1kZWZpbmF0ZWx5IGZvciBzY3JpcHQgZWFybHkgYm91bmQ=";
+            notes["objectid"] = new EntityReference("sdds_generalconfiguration", generalconfigForSecureFile.Id);
 
             // Mocking Context with your own parameters
 
@@ -48,27 +63,21 @@ namespace SDDS.UnitTests.AddressLookup
             PlugCtx.MessageName = "sdds_AddressLookup";
 
             //PlugCtx.PrimaryEntityName = "none";
-            PlugCtx.PrimaryEntityId = targetEntityId;
             PlugCtx.PrimaryEntityName = "none";
-
-
-            PlugCtx.InputParameters = inputParameter;
-            fakecontext.Initialize(new Entity()
+            var dataCollection = new List<Entity>
             {
-                Id = Guid.NewGuid(), LogicalName ="none"
-
-            });
-
+               configdata[0], configdata[1], generalconfigForSecureFile, notes
+            };
+            PlugCtx.InputParameters = inputParameter;
+            fakecontext.Initialize(dataCollection);
 
             // Act and Assert
+            var pluginres = fakecontext.ExecutePluginWith<GetAddressForPostCode>(PlugCtx);
 
-            var fakedPlugin = fakecontext.ExecutePluginWith<GetAddressForPostCode>(PlugCtx);
-            iTracing.Trace("tracee"+fakedPlugin);
-            Assert.NotNull(fakedPlugin);
+            pluginres.Should().NotBeNull();
 
-           // this.systemtestobj.Invoking(system => system.Execute(null)).Should().Throw<InvalidPluginExecutionException>();
 
-            // fakedPlugin.Should();
+
 
         }
     }
--- SDDS.UnitTests/SDDS.Workflow/Application/GetCurrentUserTests.cs
+++ SDDS.UnitTests/SDDS.Workflow/Application/GetCurrentUserTests.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using FakeXrmEasy.Abstractions;
+using FakeXrmEasy.CodeActivities;
+using FluentAssertions;
+using Microsoft.Xrm.Sdk;
+using SDDS.Workflow.Application;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SDDS.UnitTests.SDDS.Workflow.Application
 {
     public class GetCurrentUserTests : FakeXrmEasyTestsBase
     {
+        readonly IXrmFakedContext fakecontext;
+        public GetCurrentUserTests()
+        {
+            fakecontext = this.xrmFakedContext;
+
+        }
+
+
+        [Fact]
+        public void Get_current_user_test()
+        {
+            var result = fakecontext.ExecuteCodeActivity<GetCurrentUser>(fakecontext.GetDefaultWorkflowContext());
+
+            if (result != null)
+            {
+                result["CurrentUser"].Should().NotBeNull();
+            }
+
+        }
+
     }
 }
