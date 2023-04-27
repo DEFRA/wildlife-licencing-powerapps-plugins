7aea0cdb5280ddec58a3a6b842a6beccaaef30db
Victor Onyebuchi <Victor.Onyebuchi@defra.gov.uk>
2023-04-27 12:11:09 +0100

    changed sharepoint user file

---
+++ SDDS.Plugin/Model/SharePointUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDDS.Plugin.Model
+{
+    public class SharePointUser
+    {
+        public Guid Id { get; set; }
+        public bool RemoveUser { get; set; }
+        public string Name { get; set; }
+
+        public EntityReference User { get; set; }
+    }
+}
