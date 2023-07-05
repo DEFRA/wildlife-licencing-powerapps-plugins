661137e8f2cbf262372c04c7acc3b92b12303a4a
Ikenna Apeh <ikenna.apeh@defra.gov.uk>
2023-07-05 12:18:07 +0100

    HTML to word converter

---
+++ Azure/HTMLToWordConverter/HTMLToWordConverter/ConvertHTMLToWord.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Penman.Html2OpenXml;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace HTMLToWordConverter
+{
+    public static class ConvertHTMLToWord
+    {
+        [FunctionName("ConvertHTMLToWord")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["filename"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            using (MemoryStream generatedDocument = new MemoryStream())
+            {
+                using (WordprocessingDocument package = WordprocessingDocument.Create(generatedDocument, WordprocessingDocumentType.Document))
+                {
+                    MainDocumentPart mainPart = package.MainDocumentPart;
+                    if (mainPart == null)
+                    {
+                        mainPart = package.AddMainDocumentPart();
+                        new Document(new Body()).Save(mainPart);
+                    }
+
+                    HtmlConverter converter = new HtmlConverter(mainPart);
+                    converter.ParseHtml(requestBody);
+
+                    mainPart.Document.Save();
+                }
+                return new FileContentResult(generatedDocument.ToArray(), "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+                {
+                    FileDownloadName = name
+                };
+            }
+        }
+    }
+}
